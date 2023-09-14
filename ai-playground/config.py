1aafdec385819f7f111559545b59f0eda56dc186
vikyw89 <vikyw89@gmail.com>
2023-09-14 22:06:44 +0800

    feat: installed deps | added env

---
+++ ai-playground/config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.
+
+export OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
