277c4058291906e566bacd004427bc759d9db969
vikyw89 <vikyw89@gmail.com>
2023-09-15 17:57:04 +0800

    feat: gpt with ability to search internet

--- ai-playground/app.py
+++ ai-playground/app.py
@@ -1,19 +1,37 @@
+import asyncio
 from config import SECRETS
-# Import the required modules
 from langchain.llms import Clarifai
-from langchain import PromptTemplate, LLMChain
+from langchain import BasePromptTemplate, LLMChain
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+import os
+os.environ["LANGCHAIN_TRACING"] = "true" # If you want to trace the execution of the program, set to "true"
+from langchain.agents import AgentType
+from langchain.agents import initialize_agent
+from langchain.agents.agent_toolkits import PlayWrightBrowserToolkit
+from langchain.tools.playwright.utils import (
+    create_async_playwright_browser,
+    create_sync_playwright_browser, # A synchronous browser is available, though it isn't compatible with jupyter.
+)
 
-template = """Question: {question}
-
-Answer: Let's think step by step."""
-
-prompt = PromptTemplate(template=template, input_variables=["question"])
-
-clarifai_llm = Clarifai( pat=SECRETS.get("PAT"), user_id=SECRETS.get("USER_ID"), app_id=SECRETS.get("APP_ID"), model_id=SECRETS.get("MODEL_ID"))
+# This import is required only for jupyter notebooks, since they have their own eventloop
+import nest_asyncio
+nest_asyncio.apply()
 
 
-# Create LLM chain
-llm_chain = LLMChain(prompt=prompt, llm=clarifai_llm)
-question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+# This import is required only for jupyter notebooks, since they have their own eventloop
+async_browser = create_async_playwright_browser()
+browser_toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
+tools = browser_toolkit.get_tools()
 
-print(llm_chain.run(question))
+
+llm = Clarifai( pat=SECRETS.get("PAT"), user_id=SECRETS.get("USER_ID"), app_id=SECRETS.get("APP_ID"), model_id=SECRETS.get("MODEL_ID"))
+
+agent_chain = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True,)
+
+async def ask():
+    response = await agent_chain.arun(input="What's the latest xkcd comic about?")
+    print(response)
+
+
+asyncio.run(ask())
